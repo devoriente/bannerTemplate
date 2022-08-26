404fbc69de41434004ffb15e1d8fcc480acbf49f
devoriente <devoriente@outlook.com>
2022-08-27 01:08:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Je selectionne et je stocke
+//bouton .btn-success;
+const btnSuccess = document.querySelector('.btn-success');
+console.log(btnSuccess);
+//DIV cookies
+const cookies = document.querySelector('.cookies');
+console.log(cookies);
+
+btnSuccess.addEventListener('click', function(){
+    console.log('bouton cliqué !');
+    cookies.style.opacity = "0";
+});
